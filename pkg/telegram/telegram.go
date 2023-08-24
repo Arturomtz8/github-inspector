f15808f2078c3091a323e1f461fae5abbd6e0876
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2023-08-24 09:15:15 -0600

    format template

--- pkg/telegram/telegram.go
+++ pkg/telegram/telegram.go
@@ -79,17 +79,18 @@ func HandleTelegramWebhook(w http.ResponseWriter, r *http.Request) {
 
 	const templ = `{{.TotalCount}} repositories:
 	{{range .Items}}----------------------------------------
-	Name:          {{.Full_name}}
-	Url:           {{.Html_url}}
+	Name:          {{.FullName}}
+	Url:           {{.HtmlURL}}
 	Description:   {{.Description}}
-	Created at:    {{.Created_at }}
-	Update 	at:    {{.Updated_at}} 
-	Pushed at:     {{.Pushed_at}}
+  Author:        {{.Owner.Login}}
+	Created at:    {{.CreatedAt}}
+	Update at:     {{.UpdatedAt}
+	Pushed at:     {{.PushedAt}}
 	Size(KB):      {{.Size}}
 	Language:      {{.Language}}
-	Stargazers:    {{.Stargazers_count}}
+	Stargazers:    {{.StargazersCount}}
 	Archived:      {{.Archived}}
-	Open Issues:   {{.Open_issues_count}}
+	Open Issues:   {{.OpenIssuesCount}}
 	Topics:        {{.Topics}}
 	{{end}}`
 
